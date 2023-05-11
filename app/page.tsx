c86723bd9b84d0f3915b78f7cb9cdbbd1adeeb54
Renan Lopes Ferreira <relferreira@gmail.com>
2023-05-11 08:27:31 -0300

    public

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
                   >
                     <input
                       name="account"
-                      placeholder="Enter your account @..."
+                      placeholder="Enter your public account @..."
                       className="flex-1 bg-white flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                     />
                     <button

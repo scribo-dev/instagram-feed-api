e791432f837d266cd4e26cb37e7b0c5cb2b76419
Renan Lopes Ferreira <relferreira@gmail.com>
2023-07-20 14:52:11 -0300

    Adding rate limit

--- app/api/[account]/route.ts
+++ app/api/[account]/route.ts
@@ -1,27 +1,75 @@
-import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 import cors from "../../cors";
 import { scrape } from "@/lib/scrape";
 
-// export const runtime = "edge";
+import { Redis } from "@upstash/redis";
+import { Ratelimit } from "@upstash/ratelimit";
 
-// export const revalidate = 60 * 5;
+if (!process.env.KV_URL || !process.env.KV_TOKEN) throw "env not found";
+
+const redis = new Redis({
+  url: process.env.KV_URL,
+  token: process.env.KV_TOKEN,
+});
+
+const ratelimit = {
+  free: new Ratelimit({
+    redis,
+    analytics: true,
+    prefix: "ratelimit:free",
+    limiter: Ratelimit.fixedWindow(10, "10s"),
+  }),
+  paid: new Ratelimit({
+    redis,
+    analytics: true,
+    prefix: "ratelimit:paid",
+    limiter: Ratelimit.fixedWindow(60, "10s"),
+  }),
+};
+
+const clients = [
+  "935d304b135df1cbb5c5ee1fe191bfb1", //Scribo
+  "907eb8cece1b1c7a3f2ab59533041de9", // Black Swan
+];
 
 export async function GET(
-  request: Request,
+  request: NextRequest,
   {
     params,
   }: {
     params: { account: string };
   }
 ) {
+  const auth = request.headers.get("Authorization")?.replace("Bearer ", "");
+  let id = request.ip ?? "anonymous";
+  let rate = ratelimit.free;
+
+  if (auth && clients.includes(auth)) {
+    rate = ratelimit.paid;
+    id = auth;
+  }
+
+  const { success, remaining, limit, reset } = await rate.limit(
+    id ?? "anonymous"
+  );
+
+  let headers = {
+    "Content-Type": "application/json",
+    "X-RateLimit-Limit": limit.toString(),
+    "X-RateLimit-Remaining": remaining.toString(),
+    "X-RateLimit-Reset": reset.toString(),
+  };
+  if (!success)
+    return cors(request, new Response("", { status: 429, headers }));
+
   let images = await scrape(params.account);
 
   return cors(
     request,
     new Response(JSON.stringify(images), {
       status: 200,
-      headers: { "Content-Type": "application/json" },
+      headers,
     })
   );
 }

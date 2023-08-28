1904cf900850f590904b1653ed7b7783662e20aa
Renan Lopes Ferreira <relferreira@gmail.com>
2023-08-27 22:08:55 -0300

    Instagram business account link

---
+++ app/account-selector/page.tsx
@@ -0,0 +1,114 @@
+import { Metadata } from "next";
+import { prisma } from "@/lib/db";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { revalidatePath } from "next/cache";
+
+// export const runtime = "edge";
+export const dynamic = "force-dynamic";
+
+type FacebookResponse = {
+  data: FacebookAccount[];
+};
+
+type FacebookAccount = {
+  id: string;
+  name: string;
+  access_token: string;
+  instagram_business_account: {
+    id: string;
+    name: string;
+    username: string;
+  };
+};
+
+async function getData(searchParams: {
+  [key: string]: string | string[] | undefined;
+}) {
+  const res = await fetch(
+    `https://graph.facebook.com/v17.0/me/accounts?fields=id%2Cname%2Caccess_token%2Cinstagram_business_account{id,name,username}&access_token=${searchParams["long_lived_token"]}`,
+    { cache: "no-store" }
+  );
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return (await res.json()) as FacebookResponse;
+}
+
+export default async function Page({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const data = await getData(searchParams);
+
+  const alreadyConnected = await prisma.instagramAccount.findMany({
+    select: {
+      instagramName: true,
+    },
+    where: {
+      instagramName: {
+        in: data.data.map((a) => a.instagram_business_account.username),
+      },
+    },
+  });
+  console.log(alreadyConnected);
+  async function connect(formData: FormData) {
+    "use server";
+    const instagramUsername = formData.get("username") as string;
+    const token = formData.get("token") as string;
+    await prisma.instagramAccount.upsert({
+      where: { instagramName: instagramUsername },
+      create: {
+        instagramName: instagramUsername,
+        instagramAccessToken: token,
+      },
+      update: { instagramAccessToken: token },
+    });
+    revalidatePath("/account-selector");
+    // mutate data
+    // revalidate cache
+  }
+
+  return (
+    <div className="">
+      <div className="mx-auto max-w-7xl divide-y divide-gray-900/10 px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
+        <h2 className="text-2xl font-bold leading-10 tracking-tight text-gray-900">
+          Accounts
+        </h2>
+        <div className="pt-4">
+          {data?.data?.map((account) => (
+            <form key={account.id} action={connect}>
+              <div className="flex">
+                <span className="flex-1">{account.name}</span>
+                <input
+                  name="username"
+                  defaultValue={account.instagram_business_account.username}
+                  hidden
+                />
+                <input
+                  name="token"
+                  defaultValue={account.access_token}
+                  hidden
+                />
+                {alreadyConnected.find(
+                  (a) =>
+                    a.instagramName ===
+                    account.instagram_business_account.username
+                ) ? (
+                  "connected with success"
+                ) : (
+                  <button type="submit">connect</button>
+                )}
+              </div>
+            </form>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -21,13 +21,29 @@ export default async function Page({
 
   if (!session) redirect("/");
 
+  async function create(formData: FormData) {
+    "use server";
+  }
+
   return (
     <div className="">
       <div className="mx-auto max-w-7xl divide-y divide-gray-900/10 px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
         <h2 className="text-2xl font-bold leading-10 tracking-tight text-gray-900">
           Dashboard
         </h2>
-        Hello {session?.user?.name}
+
+        <div className="pt-4">
+          Hello {session?.user?.name}
+          <form action={create}>
+            <input name="account" />
+            <button type="submit">add</button>
+          </form>
+          <a
+            href={`https://www.facebook.com/v17.0/dialog/oauth?client_id=262115046660775&display=page&extras={"setup":{"channel":"IG_API_ONBOARDING"}}&redirect_uri=http://localhost:3000/auth-integration&response_type=token&scope=instagram_basic,instagram_content_publish,instagram_manage_comments,instagram_manage_insights,pages_show_list,pages_read_engagement`}
+          >
+            Login to Facebook
+          </a>
+        </div>
       </div>
     </div>
   );

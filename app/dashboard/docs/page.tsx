282c6377b73c11cb059107f78466a4029c635fc8
Renan Lopes Ferreira <relferreira@gmail.com>
2023-10-04 09:14:18 -0300

    Trying to fix docs

--- app/dashboard/docs/page.tsx
+++ app/dashboard/docs/page.tsx
@@ -2,8 +2,11 @@ import { getApiDocs } from "@/lib/swagger";
 
 import ReactSwagger from "../../api-doc/react-swagger";
 
+export const dynamic = "force-dynamic";
+
 export default async function IndexPage() {
   const spec = await getApiDocs();
+  console.log(JSON.stringify(spec));
   return (
     <section className="container">
       <ReactSwagger spec={spec} />

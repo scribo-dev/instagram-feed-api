c3f9b04f2e0318549d8391983b0a846b6a7a9a03
Renan Lopes Ferreira <relferreira@gmail.com>
2023-10-09 18:19:28 -0300

    Trying to fix docs

--- app/dashboard/docs/page.tsx
+++ app/dashboard/docs/page.tsx
@@ -1,17 +1,12 @@
 import { getApiDocs } from "@/lib/swagger";
 
 import ReactSwagger from "../../api-doc/react-swagger";
-import { Suspense } from "react";
-
-export const dynamic = "force-dynamic";
 
 export default async function IndexPage() {
   const spec = await getApiDocs();
   return (
     <section className="container">
-      <Suspense fallback={<span>"Loading..."</span>}>
-        <ReactSwagger spec={spec} />
-      </Suspense>
+      <ReactSwagger spec={spec} />
     </section>
   );
 }

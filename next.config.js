cdaf4c7f90a2a5e8e0a2bfb082f8a467680f3cc6
Renan Lopes Ferreira <relferreira@gmail.com>
2023-12-05 10:51:28 -0300

    Trying to fix build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["prisma"],
+    serverComponentsExternalPackages: ["prisma", "@temporalio/client"],
   },
   images: {
     domains: ["d2b8b46ja6xujp.cloudfront.net"],
